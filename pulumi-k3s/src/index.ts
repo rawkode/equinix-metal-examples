8a9aeea7ffda0b730511577079c9a4100da990ad
David McKay <david@rawkode.com>
2021-06-03 13:27:28 +0100

    feat: redundant k3s deployment with litestream

--- pulumi-k3s/src/index.ts
+++ pulumi-k3s/src/index.ts
@@ -1,7 +1,24 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as tls from "@pulumi/tls";
 import * as metal from "@pulumi/equinix-metal";
 
 const config = new pulumi.Config();
+const configAws = new pulumi.Config("aws");
+
+const k3sDatabaseCAKey = new tls.PrivateKey("k3s-db-ca-key", {
+  algorithm: "ECDSA",
+  ecdsaCurve: "P384",
+});
+
+const k3sDatabaseCACert = new tls.SelfSignedCert("k3s-db-ca-cert", {
+  keyAlgorithm: "ECDSA",
+  privateKeyPem: k3sDatabaseCAKey.privateKeyPem,
+  subjects: [
+    {
+      commonName: 
+    }
+  ]
+});
 
 const k3sControlPlane = new metal.Device("k3s-control-plane", {
   billingCycle: metal.BillingCycle.Hourly,
@@ -12,7 +29,32 @@ const k3sControlPlane = new metal.Device("k3s-control-plane", {
   description: "K3s Control Plane (Don't Delete)",
   metro: "am",
   userData: `#!/usr/bin/env sh
-export INSTALL_K3S_EXEC="--bind-address 127.0.0.1 --disable=traefik"
+export PRIVATE_IPv4=$(curl -s https://metadata.platformequinix.com/metadata | jq -r '.network.addresses | map(select(.public==false and .management==true)) | first | .address')
+export INSTALL_K3S_EXEC="--bind-address $PRIVATE_IPv4 --advertise-address $PRIVATE_IPv4 --node-ip $PRIVATE_IPv4 --disable=traefik"
+
+cat >/etc/litestream.yml <<END
+access-key-id: ${configAws.requireSecret("accessKey")}
+secret-access-key: ${configAws.requireSecret("secretKey")}
+
+dbs:
+  - path: /var/lib/rancher/k3s/server/db/state.db
+    replicas:
+      - url: s3://${config.require("bucketName")}/db
+
+END
+
+curl -o /tmp/litestream.deb -fsSL https://github.com/benbjohnson/litestream/releases/download/v0.3.4/litestream-v0.3.4-linux-amd64.deb
+dpkg --force-confold -i /tmp/litestream.deb
+
+litestream restore -if-replica-exists /var/lib/rancher/k3s/server/db/state.db
+
 curl -sfL https://get.k3s.io | sh -
+
+systemctl daemon-reload
+systemctl enable litestream
+systemctl start litestream
+
+kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml apply -f https://github.com/fluxcd/flux2/releases/latest/download/install.yaml
+kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml apply -f https://raw.githubusercontent.com/rawkode/equinix-metal-examples/main/pulumi-k3s/opt/flux/setup.yaml
 `,
 });
